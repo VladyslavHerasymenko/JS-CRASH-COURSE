c7b521fab6266c7e8bf5b99f61f45c86d65ee7b7
Vladyslav Herasymenko <v.v.herasymenko@globallogic.com>
2021-11-01 14:33:54 +0200

    CodeWars Kata

---
+++ CodeWars/getTheMiddleCharacter.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/56747fd5cb988479af000028/train/javascript
+
+function getMiddle(s)
+{
+  if (Math.round(s.length/2) == s.length/2) {
+    index = s.length/2;
+    result = (s[index - 1] + s[index]);
+  } else {
+    index = Math.round(s.length/2);
+    result = s[index - 1];
+  }
+  return result;
+}.log(result);
