cbadfaf1e663008ad577108b6e05bf2f8d3d15fa
Vladyslav Herasymenko <v.v.herasymenko@globallogic.com>
2021-11-01 12:17:35 +0200

    first commit

---
+++ dataTypesAndVariables/dataTypesAndVariables.js
@@ -0,0 +1,22 @@
+// primitive
+
+const one = 1;
+const name = "Vlad";
+const emptyVar = null;
+
+let varShouldBeUpdated = null;
+
+const a = "a";
+const A = "A";
+
+let temp = undefined;
+
+const obj = {
+    a: 1,
+    name: "Test",
+}
+
+const ar = [];
+
+console.log(one);
+console.log('It is name variable', name);
