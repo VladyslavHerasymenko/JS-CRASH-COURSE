138ccd500cce3fbc257588a1bfeaf12bb3bf3bd5
Vladyslav Herasymenko <v.v.herasymenko@globallogic.com>
2021-11-02 10:31:24 +0200

    CodeWars Kata

--- CodeWars/uniqueInOrder.js
+++ CodeWars/uniqueInOrder.js
@@ -1,17 +1,12 @@
 // https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
 
 var uniqueInOrder=function(iterable){
-    if (typeof iterable === 'string'){
-      iterable = iterable.split('');
-    }
-    var step;
-    var result = new Array();
+    var step
+    var result = []
     for (step = 0; step < iterable.length; step++) {
-      if (iterable[step] == iterable[step + 1]){}
-      else{
-        result.push(iterable[step]);
-        console.log(iterable[step]);
+      if (iterable[step] !== iterable[step + 1]){
+        result.push(iterable[step])
       }
     }
-    return result;
+    return result
   }
