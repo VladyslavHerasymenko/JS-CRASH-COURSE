c110425ef32e8d951496f136bdd6c2a228926e90
Vladyslav Herasymenko <v.v.herasymenko@globallogic.com>
2021-11-05 12:35:22 +0200

    HW: Data types and variables

--- dataTypesAndVariables/dataTypesAndVariables.js
+++ dataTypesAndVariables/dataTypesAndVariables.js
@@ -1,13 +1,46 @@
 /*
     Primitive date types
 */
-
+// Boolean
 const a = 1;
-let b = "Text";
-let d;
-const f = Symbol('Symbol');
+const b = 10;
 
-// Перевірки типів данних
-console.log(typeof a == b);
-console.log(typeof d);
-console.log(f.constructor);
+console.log(a == b);
+console.log(typeof (a == b));
+console.log(a < b);
+console.log(typeof (a < b));
+console.log('--------------------------------------------------');
+
+// Null
+
+const c = null;
+
+console.log(c);
+console.log(typeof (c));
+console.log('--------------------------------------------------');
+
+// Undefined
+
+let d;
+
+console.log(d);
+console.log(typeof (d));
+console.log('--------------------------------------------------');
+
+// Number
+
+const f = 10;
+const e = 0.011;
+console.log(f);
+console.log(typeof f);
+console.log(e);
+console.log(typeof e);
+console.log('--------------------------------------------------');
+
+// String
+
+const str = 'Some text';
+
+console.log(str);
+console.log(typeof str);
+console.log('--------------------------------------------------');
